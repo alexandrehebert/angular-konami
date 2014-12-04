7f53b1e9a9764bfb26d096ff52750cbae31f53b2
Alexandre HEBERT <ahebert@xebia.fr>
2014-12-04 14:44:16 +0100

    custom konami keys suite support

--- angular-konami.js
+++ angular-konami.js
@@ -1,31 +1,40 @@
-// angular-konami 0.2
-// https://github.com/dos1/angular-konami
+// angular-konami 0.3
+// https://github.com/alexandrehebert/angular-konami
+// based on https://github.com/dos1/angular-konami
 // based on https://gist.github.com/benajnim/5238495
-angular.module('konami', []).directive("konami", ['$document', function($document) {
-        return {
-            restrict: 'A',
-            scope: {
-                konami: '&'
-            },
-            link: function(scope) {
-                var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], konami_index = 0;
+angular.module('konami', []).directive("konami", ['$document', function ($document) {
 
-                var handler = function(e) {
-                    if (e.keyCode === konami_keys[konami_index++]) {
-                        if (konami_index === konami_keys.length) {
-                            $document.off('keydown', handler);
-                            scope.$apply(scope.konami);
-                        }
-                    } else {
-                        konami_index = 0;
-                    }
-                };
+    function link(scope, element, attrs) {
 
-                $document.on('keydown', handler);
+        var konamiKeys = angular.isUndefined(attrs.keys)
+            ? attrs.keys : [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+        var konamIndex = 0;
 
-                scope.$on('$destroy', function() {
-                    $document.off('keydown', handler);
-                });
+        var handler = function (event) {
+            var keyCode = event.keyCode ? event.keyCode : event.which;
+            if (keyCode === konamiKeys[konamIndex++]) {
+                if (konamIndex === konamiKeys.length) {
+                    scope.$apply(scope.konami);
+                }
+            } else {
+                konamIndex = 0;
             }
         };
-    }]);
+
+        $document.on('keydown', handler);
+
+        scope.$on('$destroy', function () {
+            $document.off('keydown', handler);
+        });
+
+    }
+
+    return {
+        restrict: 'A',
+        scope: {
+            konami: '&'
+        },
+        link: link
+    };
+
+}]);
